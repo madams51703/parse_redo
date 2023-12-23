d8af5037f2b9b8823707a3a77202384808658e5a
Mark <madams51703@gmail.com>
2023-12-23 17:39:47 -0500

    Added main cpp code and new sample input

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <antlr4-runtime/antlr4-runtime.h>
+
+#include "RedoLexer.h"
+#include "RedoParser.h"
+#include "RedoWalker.h"
+
+using namespace antlr4;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string line;
+	ifstream RedoParserFile ("msa.input");
+	if (RedoParserFile.is_open()) {
+		ANTLRInputStream input(RedoParserFile);
+		RedoLexer lexer(&input);
+		CommonTokenStream tokens(&lexer);
+
+		tokens.fill();
+		for (auto token : tokens.getTokens()) {
+//			std::cout << token->toString() << std::endl;
+		}
+
+		RedoParser parser(&tokens);
+
+
+
+		antlr4::tree::ParseTreeWalker walker;	
+		antlr4::ParserRuleContext* redo_file = parser.redo_file();
+ 		RedoWalker * listener = new RedoWalker();		
+  		walker.walk(listener, redo_file);
+		RedoParserFile.close();
+	}
+}
