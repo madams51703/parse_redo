6d1ce136356503063b1e00e827aa4ba816b83e99
Mark <madams51703@gmail.com>
2023-12-22 21:15:25 -0500

    Modified to build on latest antlr4

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir -p ./org/antlr/v4/runtime/
-java -jar ./antlr4-4.7.3-complete.jar RedoParser.g4 RedoLexer.g4
+java -jar ./antlr-4.13.1-complete.jar RedoParser.g4 RedoLexer.g4
 
-javac -cp ./antlr4-4.7.3-complete.jar:./org/antlr/v4/runtime *.java
+javac -cp ./antlr-4.13.1-complete.jar:./org/antlr/v4/runtime *.java
 cp CaseChangingCharStream.class ./org/antlr/v4/runtime/.
 jar -cvf redo.jar *.class ./org/antlr/v4/runtime/*.class
 rm *.class 
